c6b859c5bced7b2b69b1f48bb4d32dac2a8ac739
남은솔 <106670648+nameunsol@users.noreply.github.com>
2025-06-10 01:31:53 +0900

    아이템 스폰 수정 완료

---
+++ Assets/TextMesh Pro/Script/Home Portal.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomePortal : MonoBehaviour
+{
+    public Transform mapPos;
+    public GameObject player;
+    public GameObject Camera;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player")) 
+        {
+
+            Debug.Log("캐릭터 이동");
+            CharacterController controller = player.GetComponent<CharacterController>();
+
+            Vector3 pos = mapPos.position;
+
+            controller.enabled = false;
+            player.transform.position = pos;
+            controller.enabled = true;
+            Camera.transform.position = pos;
+
+            Debug.Log("캐릭터 이동 완료");
+        }
+    }
+}
