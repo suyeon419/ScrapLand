33d6e8f132afb15f6d21f22bc04897e6a3f3c276
hlj1085 <hlj1085@naver.com>
2025-06-15 23:27:16 +0900

    Revert "기계부분 수정"
    This reverts commit 4b40bebaa7fd7035c1802a97aa7a2098a77be187.

--- Assets/TextMesh Pro/Script/Test.cs
+++ Assets/TextMesh Pro/Script/Test.cs
@@ -8,10 +8,12 @@ public class Test : MonoBehaviour
 
     void Update()
     {
+        /*
         if (!test)
         {
-            PlacementManager.Instance.SetHeldItem("BlastFurnace");
+            PlacementManager.Instance.SetHeldItem("Clock");
             test = true;
         }
+        */
     }
 }
