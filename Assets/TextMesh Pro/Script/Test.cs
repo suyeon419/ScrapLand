0b4ebd6f6ad7c1485b2d89355a75d9d7305edcfa
남은솔 <106670648+nameunsol@users.noreply.github.com>
2025-06-16 12:25:08 +0900

    테스트코드 닫음

--- Assets/TextMesh Pro/Script/Test.cs
+++ Assets/TextMesh Pro/Script/Test.cs
@@ -8,10 +8,12 @@ public class Test : MonoBehaviour
 
     void Update()
     {
+        /*
         if (!test)
         {
             PlacementManager.Instance.SetHeldItem("Grinder");
             test = true;
         }
+        */
     }
 }
